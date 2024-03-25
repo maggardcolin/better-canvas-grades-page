d30f1efd077db0388617749282f1e5d840146b2d
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2024-03-24 19:01:58 -0500

    updates

--- content.js
+++ content.js
@@ -1,35 +1,53 @@
 function calculateAndDisplayGrades() {
 
-    console.log("Script loaded!");
-
-    // create summary dictionary for each category weight
+    // find categories and percentages and create dictionary for each category
     const summaryTable = document.querySelector('.summary');
+    const summaryBody = summaryTable.querySelector('tbody');
+    const categories = summaryBody.querySelectorAll('tr');
+    let categoryDetails = [];
+    categories.forEach(category => {
+        const title = category.textContent.split("\n")[1].trim();
+        const percentage = category.textContent.split("\n")[2].trim();
+        let percentageValue = parseFloat(percentage.replace('%', ''));
+        let percentageDecimal = Math.round((percentageValue / 100) * 10) / 10;
 
-    // find all mean grades, append them to the correct mean category array
+        // create a dictionary where there is a title, percentage, and total field
+        let categoryDict = {
+            title: title,
+            percentage: percentageDecimal,
+            totalPoints: 0
+        };
+        if (title !== 'Total') {
+            categoryDetails.push(categoryDict);
+        }
+    });
+    console.log(categoryDetails);
 
-    // find all median grades, append them to the correct median category array
-  
+    // find all mean grades, append points to the correct mean point total
+
+    // find all median grades, append points to the correct median point total
+
     // calculate the mean grade in the class
 
     // calculate the median grade in the class
 
     let mean = 1;
     let median = 2;
-  
+
     displayResults(mean, median);
 }
 
 function displayResults(mean, median) {
-  const resultsContainer = document.querySelector('.final_grade');
-  resultsContainer.append("Mean: ");
-  const meanSpan = document.createElement('span');
-  meanSpan.textContent = mean.toFixed(2);;
-  resultsContainer.append(meanSpan);
-  resultsContainer.append("Median: ");
-  const medianSpan = document.createElement('span');
-  medianSpan.textContent = median.toFixed(2);;
-  resultsContainer.append(medianSpan);
+    const resultsContainer = document.querySelector('.final_grade');
+    resultsContainer.textContent = '';
+    resultsContainer.append("Mean: ");
+    const meanSpan = document.createElement('span');
+    meanSpan.textContent = mean.toFixed(2);;
+    resultsContainer.append(meanSpan);
+    resultsContainer.append("Median: ");
+    const medianSpan = document.createElement('span');
+    medianSpan.textContent = median.toFixed(2);;
+    resultsContainer.append(medianSpan);
 }
 
-// Run the calculation when the page is loaded
-window.addEventListener('load', calculateAndDisplayGrades);
+calculateAndDisplayGrades();
